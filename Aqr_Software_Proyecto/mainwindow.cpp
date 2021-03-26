9ba31119e3e3d2b89be30cc333ad0046f438a251
Mario <mayitoalmeidamc@gmail.com>
2021-03-26 02:54:51 -0600

    first commit

--- Aqr_Software_Proyecto/mainwindow.cpp
+++ Aqr_Software_Proyecto/mainwindow.cpp
@@ -13,3 +13,22 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_pushButton_clicked()
+{
+    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setHostName("127.0.0.1");
+    db.setUserName("root");
+    db.setPassword("mayitofirst01");
+    db.setDatabaseName("cashflow_db");
+
+    if(db.open()){
+
+        QMessageBox::information(this, "Connection", "Database Connected Successfully");
+
+    } else{
+        QSqlError error = db.lastError();
+                QMessageBox::information(this, "Connection", error.databaseText());
+
+    }
+}
